86b94b604106d3b20967a504aeb282debd781cd7
cyanmallow <ggmmallow@gmail.com>
2024-03-18 22:36:17 +0700

    finally made some progress on tictactoe

--- ticktactoe/index.js
+++ ticktactoe/index.js
@@ -1,16 +1,41 @@
 function getComputerChoice(){
-    computerSelection = RTCEncodedAudioFrame(rock, paper, scissor)
-    return computerSelection;
+    const randomNumber = Math.floor(Math.random() *3);
+    const options = ["rock", "paper", "scissors"];
+    return options[randomNumber];
 }
 
-function payRound(playerSelection, computerSelection){
-    if (playerSelection == paper){
+console.log(getComputerChoice());
 
+
+document.getElementById('rock').addEventListener('click', function(){
+    const computerChoice = getComputerChoice();
+    if (computerChoice == 'rock') {
+        alert('Draw!!');
+    } else if (computerChoice == 'paper'){
+        alert('You win!!');
+    } else {
+        alert("You lose :(");
     }
-}
+});
 
-function playGame(){
-    for (int i=0; i<5; i++){
-        
+document.getElementById('paper').addEventListener('click', function(){
+    const computerChoice = getComputerChoice();
+    if (computerChoice == 'paper') {
+        alert('Draw!!');
+    } else if (computerChoice == 'scissors'){
+        alert('You win!!');
+    } else {
+        alert("You lose :(");
     }
-}
+});
+
+document.getElementById('scissors').addEventListener('click', function(){
+    const computerChoice = getComputerChoice();
+    if (computerChoice == 'scissors') {
+        alert('Draw!!');
+    } else if (computerChoice == 'rock'){
+        alert('You win!!');
+    } else {
+        alert("You lose :(");
+    }
+});
